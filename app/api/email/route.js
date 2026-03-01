556a7c8b90a162d57878a8f20726330e0929203f
Param Sab <param@example.com>
2026-03-01 10:50:22 +0530

    chore: migrate to postgres and refactor email for production

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend('re_dKg6GiKk_JthRfSy4BQ1yHvDeKYfirGqe');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
     try {
